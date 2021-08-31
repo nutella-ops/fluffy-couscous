b131e64bb1b3f1f0d10232c3c61d22bf663b5b24
higg <higginshiggins2@protonmail.com>
2021-08-31 11:26:58 -0700

    aadded print files on sd to serial and unsuccessfully tried 16bit/16kHz file

--- test/test.ino
+++ test/test.ino
@@ -1,6 +1,6 @@
 #include <SD.h>                      // need to include the SD library
 //#define SD_ChipSelectPin 53  //example uses hardware SS pin 53 on Mega2560
-#define SD_ChipSelectPin 10  //using digital pin 4 on arduino nano 328, can use other pins
+// #define SD_ChipSelectPin 10  //using digital pin 4 on arduino nano 328, can use other pins
 #include <TMRpcm.h>           //  also need to include this library...
 #include <pcmRF.h>
 #include <pcmConfig.h>
@@ -11,17 +11,41 @@ TMRpcm audio;   // create an object for use in this sketch
 Sd2Card card;
 SdVolume volume;
 SdFile root;
+const int chipSelect = 10;
 
 void setup(){
 
   audio.speakerPin = 9; //5,6,11 or 46 on Mega, 9 on Uno, Nano, etc
 
-  Serial.begin(9600);
-  if (!SD.begin(SD_ChipSelectPin)) {  // see if the card is present and can be initialized:
+  Serial.begin(115200);
+  if (!SD.begin(chipSelect)) {  // see if the card is present and can be initialized:
     Serial.println("SD fail");  
     return;   // don't do anything more if not
   }
-//   audio.play("tree.wav"); //the sound file "music" will play each time the arduino powers up, or is reset
+  audio.setVolume(1);
+  audio.quality(2);
+
+   // we'll use the initialization code from the utility libraries
+  // since we're just testing if the card is working!
+  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("* is a card inserted?");
+    Serial.println("* is your wiring correct?");
+    Serial.println("* did you change the chipSelect pin to match your shield or module?");
+    while (1);
+  } else {
+    Serial.println("Wiring is correct and a card is present.");
+  }
+
+   // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
+  if (!volume.init(card)) {
+    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+    while (1);
+  }
+
+  // list all files in the card with date and size
+  root.openRoot(volume);
+  root.ls(LS_R | LS_DATE | LS_SIZE);
 }
 
 
@@ -30,8 +54,11 @@ void loop(){
 
   if(Serial.available()){    
     if(Serial.read() == 'p'){ //send the letter p over the serial monitor to start playback
-      audio.play("tree.wav");
-    }
+      audio.play("tree16.wav");
   }
 
+   }
+  if(Serial.read() == 's'){ //send the letter p over the serial monitor to start playback
+  audio.stopPlayback();
+  }
 }
