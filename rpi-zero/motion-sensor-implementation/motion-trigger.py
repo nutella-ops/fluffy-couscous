77142e16ccaf3aba4f967e8dea32ebfd5d5467dc
higg <higginshiggins2@protonmail.com>
2021-10-03 22:11:48 -0700

    led assignment didn't work

--- rpi-zero/motion-sensor-implementation/motion-trigger.py
+++ rpi-zero/motion-sensor-implementation/motion-trigger.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
 
-import RPi.GPIO as GP
+import RPi.GPIO as GPIO
 import time
 
-GP.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(12, GPIO.IN) # phys 32
+GPIO.setup(20, GPIO.OUT) # phys 38
+
+
+led = GPIO.output(20, True)
+
+while True:
+	GPIO.output(20, True)
+	time.sleep(1)
+	GPIO.output(20, False)
+	time.sleep(1)
+
+	# motionSensor = GPIO.input(12)
+	# if motionSensor == True:
+	# 	GPIO.output(20, motionSensor)
