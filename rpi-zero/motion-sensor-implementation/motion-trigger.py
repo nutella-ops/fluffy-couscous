e0bfb645e11e465801b2f346fb3780bf75671d9d
higg <higginshiggins2@protonmail.com>
2021-10-05 02:22:59 -0700

    added pre & post delays and  retrigger during delay has no effect

--- rpi-zero/motion-sensor-implementation/motion-trigger.py
+++ rpi-zero/motion-sensor-implementation/motion-trigger.py
@@ -12,10 +12,8 @@ pirSensor = 20
 gpio.setup(led, gpio.OUT) # phys 32
 gpio.setup(pirSensor, gpio.IN) # phys 38
 
-# while True:
-# 	if gpio.input(pirSensor) == True:
-# 		gpio.output(led, True)
-# 	else:
-# 		gpio.output(led, False)
-
-os.system('aplay /usr/share/sounds/alsa/Front_Center.wav')
+while True:
+	if gpio.input(pirSensor) == True:
+		time.sleep(2)	# 2 second delay in audio output so audio doesn't start immedately on movement 
+		os.system('aplay /usr/share/sounds/alsa/Front_Center.wav')
+		time.sleep(40)	# 40 second delay to prevent immediate retriggering
