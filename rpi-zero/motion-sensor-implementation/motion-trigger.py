70a742b6c26d27faeca78c46fbb7361622d1387d
higg <higginshiggins2@protonmail.com>
2021-10-05 01:22:25 -0700

    played test wav file using os.system

--- rpi-zero/motion-sensor-implementation/motion-trigger.py
+++ rpi-zero/motion-sensor-implementation/motion-trigger.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-import RPi.GPIO as gpio
-import time
+import RPi.GPIO as gpio, time, os
 
 gpio.setmode(gpio.BCM)
 
@@ -13,8 +12,10 @@ pirSensor = 20
 gpio.setup(led, gpio.OUT) # phys 32
 gpio.setup(pirSensor, gpio.IN) # phys 38
 
-while True:
-	if gpio.input(pirSensor) == True:
-		gpio.output(led, True)
-	else:
-		gpio.output(led, False)
+# while True:
+# 	if gpio.input(pirSensor) == True:
+# 		gpio.output(led, True)
+# 	else:
+# 		gpio.output(led, False)
+
+os.system('aplay /usr/share/sounds/alsa/Front_Center.wav')
