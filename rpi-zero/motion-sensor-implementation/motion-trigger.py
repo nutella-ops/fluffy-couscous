2609e9a17e6d7a43f020ddb7bb83182e07343f6e
higg <higginshiggins2@protonmail.com>
2021-10-05 14:53:13 -0700

    renamed led to SPDT

--- rpi-zero/motion-sensor-implementation/motion-trigger.py
+++ rpi-zero/motion-sensor-implementation/motion-trigger.py
@@ -5,21 +5,21 @@ import RPi.GPIO as gpio, time, os
 gpio.setmode(gpio.BCM)
 
 # give pin numbers a name
-led = 12
+SPDT = 12
 pirSensor = 20
 
 # I/O definitions
-gpio.setup(led, gpio.IN) # phys 32
+gpio.setup(SPDT, gpio.IN) # phys 32
 gpio.setup(pirSensor, gpio.IN) # phys 38
 
 while True:
 	if gpio.input(pirSensor) == True:
-		if gpio.input(led) == False:
+		if gpio.input(SPDT) == False:
 			time.sleep(2)	# 2 second delay in audio output so audio doesn't start immedately on movement 
 			os.system('aplay ~/full-deutsch.wav')
 			time.sleep(40)	# 40 second delay to prevent immediate retriggering
 		
-		if gpio.input(led) == True:
+		if gpio.input(SPDT) == True:
 			time.sleep(2)	# 2 second delay in audio output so audio doesn't start immedately on movement 
 			os.system('aplay ~/full-english.wav')
 			time.sleep(40)	# 40 second delay to prevent immediate retriggering
