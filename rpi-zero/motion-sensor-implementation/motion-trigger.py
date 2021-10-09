24f7ff26f4ad94b7d68da6157ceb25593616bbbe
higg <higginshiggins2@protonmail.com>
2021-10-09 02:23:36 -0700

    runs but skips the green/yellow flashing portion

--- rpi-zero/motion-sensor-implementation/motion-trigger.py
+++ rpi-zero/motion-sensor-implementation/motion-trigger.py
@@ -4,26 +4,43 @@ import RPi.GPIO as gpio, time, os
 
 gpio.setmode(gpio.BCM)
 
-# Name the Pins
+# Named the Pins (BCM referenced)
 languageSelect = 12
 pirSensor = 20
+ledGreen = 8
+ledYellow = 10
 
 # Time Definitions
-calibDelay = 45	# min. suggested = 30 seconds, max. suggested = 60
+calibDelay = 45	# suggested calibration times {min. = 30 seconds, max. = 60} ideally, there should be as little movement as possible during calibration 
 preDelay = 2	# delay before audio starts
 postDelay = 40	# delay before audio can start again
+delayCounter = 45 # calibration delay stored in a variable to be counted
 
 # I/O definitions
 gpio.setup(languageSelect, gpio.IN) # phys 32
 gpio.setup(pirSensor, gpio.IN) # phys 38
-left = False
-right = True
-
+gpio.setup(ledGreen, gpio.OUT)
+gpio.setup(ledYellow, gpio.OUT)
 
 # Setup Dialog
+def tenSecond(pin):
+		gpio.output(pin, True)
+		time.sleep(0.5)
+		gpio.output(pin, False)
+
 print("Calibrating...")
-time.sleep(calibDelay)
+if delayCounter != 0:
+	while calibDelay <= 10:
+		tenSecond(ledGreen)
+		tenSecond(ledYellow)
+	delayCounter -= 1
+gpio.output(ledGreen, False)
+gpio.output(ledYellow, False)
 print("Calibration Complete.")
+
+# Named the language switch positions
+left = False
+right = True
 
 while True:
 	if gpio.input(pirSensor) == True:
