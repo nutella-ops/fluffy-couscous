c0988f29f2d146236eea2db32009283495caf89e
higg <higginshiggins2@protonmail.com>
2021-10-09 21:43:27 -0700

    operational simplified indicator system

--- rpi-zero/motion-sensor-implementation/motion-trigger.py
+++ rpi-zero/motion-sensor-implementation/motion-trigger.py
@@ -11,11 +11,16 @@ ledRed = 23 # phys 16
 ledGreen = 24 # phys 18
 
 # Time Definitions
-calibTotal = 4	# suggested calibration times {min. = 30 seconds, max. = 60} ideally, there should be as little movement as possible during calibration 
+calibTime = 4	# suggested calibration times {min. = 30 seconds, max. = 60} ideally, there should be as little movement as possible during calibration 
 preDelay = 3	# delay before audio starts
-postDelay = 4	# delay before audio can start again
-calibRemain = 2 # the length of led indication in seconds
-calibMain = abs(calibTotal - calibRemain) # setting aside 10 seconds for led indication, assigning the remainder as the calibMain
+retrigDelay = 4	# delay before audio can start again
+
+# Calibration Functon
+def calib(t):
+	while t > 0:
+		time.sleep(1)
+		print(t)
+		t -= 1
 
 # I/O definitions
 gpio.setup(languageSelect, gpio.IN) # phys 32
@@ -23,55 +28,43 @@ gpio.setup(pirSensor, gpio.IN) # phys 38
 gpio.setup(ledGreen, gpio.OUT)
 gpio.setup(ledRed, gpio.OUT)
 
-# Setup Dialog
-def blink(pin):
-		gpio.output(pin, True)
-		time.sleep(1)
-		gpio.output(pin, False)
-
-def ledIndicator(time, color):
-	if time <= 10:
-		while time > 0:
-			blink(color)
-			time -= 1
-
 # Start-up Conditions
 gpio.output(ledRed, False)
 gpio.output(ledGreen, False)
 
+
 ### MAIN PROGRAM ###
 
 # Initalization Display
-print("Calibrating...")
 gpio.output(ledRed, True)
-
-while calibMain > 0:
-       time.sleep(1)
-       print(calibMain)
-       calibMain -= 1
+print("Calibrating...")
+calib(calibTime)
 print("Calibration Complete.")
 gpio.output(ledRed, False)
-
-# gpio.output(ledGreen, True)
+gpio.output(ledGreen, True)
 
 # Named the language switch positions
 left = False
 right = True
-# gpio.output(ledGreen, False)	# turn off led to indicate trigger
+
 while True:
 	if gpio.input(pirSensor) == True:
 		if gpio.input(languageSelect) == left:
+			gpio.output(ledGreen, True)
 			time.sleep(preDelay)	# 2 second delay in audio output so audio doesn't start immedately on movement
+			gpio.output(ledGreen, False)
 			gpio.output(ledRed, True)	# turn red led on to inidicate triggering is inactive 
 			os.system('aplay /usr/share/sounds/alsa/Front_Center.wav')
-			gpio.output(ledRed, False)
-			# time.sleep(postDelay)	# delay to prevent immediate retriggering
-			ledIndicator(calibRemain, ledRed)	# flash led for for (delayRemain) seconds 
+			time.sleep(retrigDelay)	# delay to prevent immediate retriggering
+			gpio.output(ledRed, False)	# turn red led on to inidicate triggering is active 
+			gpio.output(ledGreen, True)
 		
 		if gpio.input(languageSelect) == right:
+			gpio.output(ledGreen, True)
 			time.sleep(preDelay)	# 2 second delay in audio output so audio doesn't start immedately on movement
+			gpio.output(ledGreen, False)
 			gpio.output(ledRed, True)	# turn red led on to inidicate triggering is inactive 
 			os.system('aplay /usr/share/sounds/alsa/Rear_Center.wav')
-			gpio.output(ledRed, False)
-			# time.sleep(postDelay)	# delay to prevent immediate retriggering
-			ledIndicator(calibRemain, ledRed)	# flash led for for (delayRemain) seconds 
+			time.sleep(retrigDelay)	# delay to prevent immediate retriggering
+			gpio.output(ledRed, False)	# turn red led on to inidicate triggering is active 
+			gpio.output(ledGreen, True)
