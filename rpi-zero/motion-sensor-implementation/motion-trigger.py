edbb49e80dd90193d8357ff02a26ec9b72c1f5d4
higg <higginshiggins2@protonmail.com>
2021-10-12 18:06:10 -0700

    added flashing calibration light and renamed audio definitions

--- rpi-zero/motion-sensor-implementation/motion-trigger.py
+++ rpi-zero/motion-sensor-implementation/motion-trigger.py
@@ -6,8 +6,8 @@ import RPi.GPIO as gpio, time, os
 gpio.setmode(gpio.BCM)
 
 # Audio File Definitions
-deutsch = 'full-deutsch.wav'
-english = 'full-english.wav' 
+DE = 'full-deutsch.wav'
+EN = 'full-english.wav' 
 
 # Named the Pins (BCM referenced)
 languageSelect = 12
@@ -20,12 +20,6 @@ calibTime = 4	# suggested calibration times {min. = 30 seconds, max. = 60} ideal
 preDelay = 3	# delay before audio starts
 retrigDelay = 4	# delay before audio can start again
 
-# Calibration Functon
-def calib(t):
-	while t > 0:
-		time.sleep(1)
-		t -= 1
-
 # I/O definitions
 gpio.setup(languageSelect, gpio.IN) # phys 32
 gpio.setup(pirSensor, gpio.IN) # phys 38
@@ -37,10 +31,35 @@ gpio.output(ledRed, False)
 gpio.output(ledGreen, False)
 
 
+### FUNCTION DEFINITIONS ###
+
+# Calibration Functon
+def calib(t):
+	while t > 0:
+		gpio.output(ledRed, True)
+		time.sleep(0.5)
+		gpio.output(ledRed, False)
+		time.sleep(0.5)
+		t -= 1
+
+
+# Motion Greeting and Status Indicator
+# def greeting(audio)
+# 	gpio.output(ledGreen, True) # continue LED green from initialization?
+# 	time.sleep(preDelay)	# 2 second delay in audio output so audio doesn't start immedately on movement
+# 	gpio.output(ledGreen, False)
+# 	gpio.output(ledRed, True)	# turn red led on to inidicate triggering is inactive 
+# 	os.system('aplay /home/pi/' + audio)
+# 	time.sleep(retrigDelay)	# delay to prevent immediate retriggering
+# 	gpio.output(ledRed, False)	# turn red led on to inidicate triggering is active 
+# 	gpio.output(ledGreen, True)	# 
+
+
+
 ### MAIN PROGRAM ###
 
 # Initalization Display
-gpio.output(ledRed, True)
+gpio.output(ledRed, True) 
 print("Calibrating...")
 calib(calibTime)
 print("Calibration Complete.")
@@ -58,7 +77,7 @@ while True:
 			time.sleep(preDelay)	# 2 second delay in audio output so audio doesn't start immedately on movement
 			gpio.output(ledGreen, False)
 			gpio.output(ledRed, True)	# turn red led on to inidicate triggering is inactive 
-			os.system('aplay /home/pi/' + deutsch)
+			os.system('aplay /home/pi/' + DE)
 			time.sleep(retrigDelay)	# delay to prevent immediate retriggering
 			gpio.output(ledRed, False)	# turn red led on to inidicate triggering is active 
 			gpio.output(ledGreen, True)
@@ -67,8 +86,8 @@ while True:
 			gpio.output(ledGreen, True)
 			time.sleep(preDelay)	# 2 second delay in audio output so audio doesn't start immedately on movement
 			gpio.output(ledGreen, False)
-			gpio.output(ledRed, True)	# turn red led on to inidicate triggering is inactive 
-			os.system('aplay /home/pi/' + english)
+			gpio.output(ledRed, True)	# turn red led on to inidicate triggering is inactive
+			os.system('aplay /home/pi/' + EN)
 			time.sleep(retrigDelay)	# delay to prevent immediate retriggering
-			gpio.output(ledRed, False)	# turn red led on to inidicate triggering is active 
+			gpio.output(ledRed, False)	# turn red led on to inidicate triggering is active
 			gpio.output(ledGreen, True)
